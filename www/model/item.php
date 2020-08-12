86a13ecf66cfd7b2815c81dd02a4d7ca06f257cb
YukizzZ <yuki.tennis2588@icloud.com>
2020-08-11 19:08:25 -0700

    delete sql injection

--- www/model/item.php
+++ www/model/item.php
@@ -82,14 +82,8 @@ function insert_item($db, $name, $price, $stock, $filename, $status){
         image,
         status
       )
-    VALUES(?,?,?,?,?);
+    VALUES('{$name}', {$price}, {$stock}, '{$filename}', {$status_value});
   ";
-  $stmt = $db->prepare($sql);
-  $stmt->bindValue(1, $name,    PDO::PARAM_STR);
-  $stmt->bindValue(2, $price,    PDO::PARAM_INT);
-  $stmt->bindValue(3, $stock,    PDO::PARAM_INT);
-  $stmt->bindValue(4, $filename,    PDO::PARAM_STR);
-  $stmt->bindValue(5, $status_value,    PDO::PARAM_STR);
 
   return execute_query($db, $sql);
 }
